c5ebc4d19ac8a8e736ae2b27b1d5d03d88a0bec6
Glenn <mr.glennichou@gmail.com>
2021-08-16 15:43:56 +0200

    module one test commit

---
+++ composed_article/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Composed Articles",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Glenn Crompton",
+    'category': 'Inventory',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
