75db74d27e3100aff346106432765f24096afa58
Glenn <mr.glennichou@gmail.com>
2021-08-29 15:14:35 +0200

    Moved to directory

---
+++ composed_article/composed-article/__manifest__.py
@@ -0,0 +1,57 @@
+{
+    'name': "Composed Articles",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Glenn Crompton",
+    'category': 'Inventory',
+    'description': """
+    Module built to make articles composed so that one article can be composed of another
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
+    'name': 'Odoo 14 Development Tutorials',
+    'version': '14.0.2.1.0',
+    'summary': 'Odoo 14 Development Tutorials',
+    'sequence': -100,
+    'description': """Odoo 14 Development Tutorials""",
+    'category': 'Tutorials',
+    'author': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'website': 'https://www.odoomates.tech',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'mail',
+        'website_slides',
+        'hr',
+        'report_xlsx'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'data/slide_data_v12.xml',
+        # 'data/slide_data_v12_2.xml',
+        'data/slide_data_v13.xml',
+        'data/slide_data_v14.xml',
+        'wizard/create_appointment_view.xml',
+        'wizard/search_appointment_view.xml',
+        'views/patient_view.xml',
+        'views/doctor_view.xml',
+        'views/appointment_view.xml',
+        'views/kids_view.xml',
+        'views/patient_gender_view.xml',
+        'views/sale.xml',
+        'report/patient_details_template.xml',
+        'report/patient_card.xml',
+        'report/report.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
