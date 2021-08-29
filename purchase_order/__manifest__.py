ed82ec4dd7fb4309571fa808948791e6571a1f75
Glenn <mr.glennichou@gmail.com>
2021-08-29 16:32:42 +0200

    Adding dependencies

--- purchase_order/__manifest__.py
+++ purchase_order/__manifest__.py
@@ -1,14 +1,14 @@
 {
     'name': "Location purchase order",
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['base', 'sale_rental'],
     'author': "Glenn Crompton",
     'category': 'Inventory',
     'description': """
    '
     """,
     # data files always loaded at installation
-    'data': [],
+    'data': ['reports/report_button.xml'],
     'active' : 'False',
     # data files containing optionally loaded demonstration data
     'demo': [],
