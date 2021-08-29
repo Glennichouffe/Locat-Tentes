7bd1e8349ad04cd19b6799a6be9f864b7fa14c6e
Glenn <mr.glennichou@gmail.com>
2021-08-29 16:20:43 +0200

    Test commit for module building

--- purchase-order/__manifest__.py
+++ purchase-order/__manifest__.py
@@ -1,14 +1,14 @@
 {
     'name': "Location purchase order",
     'version': '1.0',
-    'depends': ['base', 'sale', 'sale.order'],
+    'depends': ['base'],
     'author': "Glenn Crompton",
     'category': 'Inventory',
     'description': """
    '
     """,
     # data files always loaded at installation
-    'data': ['reports/report-button.xml'],
+    'data': [],
     'active' : 'False',
     # data files containing optionally loaded demonstration data
     'demo': [],
