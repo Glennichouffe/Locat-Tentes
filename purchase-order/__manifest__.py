9024cdf18a5755ea163add4fa16b723766207a2c
Glenn <mr.glennichou@gmail.com>
2021-08-29 15:43:04 +0200

    Adding pdf button

---
+++ purchase-order/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Location purchase order",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Glenn Crompton",
+    'category': 'Inventory',
+    'description': """
+   
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+}
+    
