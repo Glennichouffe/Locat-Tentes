c39dc96e745977816598f2464c87f5e253abf6f3
Glenn <mr.glennichou@gmail.com>
2021-08-29 16:45:37 +0200

    Typo changes

--- purchase_order/__manifest__.py
+++ purchase_order/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': "Location purchase order",
     'version': '1.0',
-    'depends': ['base', 'sale_rental'],
+    'depends': ['base', 'sale_renting'],
     'author': "Glenn Crompton",
     'category': 'Inventory',
     'description': """
