f645bcee90fda9f0a6e8eb120d0893799a759a3e
Glenn <mr.glennichou@gmail.com>
2021-09-01 19:26:42 +0200

    Adding view

--- invoice_price_double/models/price_with_installation.py
+++ invoice_price_double/models/price_with_installation.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models
 class PriceWithInstallation(models.Model):
     _inherit = "product.template"
 
-    price_with_installation = fields.Char(string='Prix avec l\'installation')
+    price_with_installation = fields.Float(string='Prix avec l\'installation')
