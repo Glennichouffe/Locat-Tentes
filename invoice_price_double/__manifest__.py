24e224d5592981602391b84ad07358ef70e654bd
Glenn <mr.glennichou@gmail.com>
2021-09-01 19:12:18 +0200

    manifest modifications

--- invoice_price_double/__manifest__.py
+++ invoice_price_double/__manifest__.py
@@ -1,24 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'Odoo 14 Development Tutorials',
-    'version': '14.0.2.1.0',
-    'summary': 'Odoo 14 Development Tutorials',
-    'sequence': -100,
-    'description': """Odoo 14 Development Tutorials""",
-    'category': 'Tutorials',
-    'author': 'Odoo Mates',
-    'maintainer': 'Odoo Mates',
-    'website': 'https://www.odoomates.tech',
-    'license': 'AGPL-3',
+    'name': "Second price for inventory",
+    'version': '1.0',
     'depends': [
+        'base',
         'stock', 
         'product'
     ],
-    'data': [],
+    'author': "Glenn Crompton",
+    'category': 'Renting',
+    'description': """
+   '
+    """,
+    # data files always loaded at installation
+    'data': [
+    'reports/sale_order_detail.xml',
+    'reports/report_button.xml'
+    ],
+    'active' : 'False',
+    # data files containing optionally loaded demonstration data
     'demo': [],
-    'qweb': [],
-    'images': ['static/description/banner.gif'],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
 }
