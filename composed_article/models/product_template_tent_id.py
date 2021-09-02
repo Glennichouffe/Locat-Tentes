478d865c755e546044526ade3cdeb66f48c9c4c0
Glenn <mr.glennichou@gmail.com>
2021-09-02 19:02:11 +0200

    many2one

--- composed_article/models/product_template_tent_id.py
+++ composed_article/models/product_template_tent_id.py
@@ -5,5 +5,4 @@ class ProductTemplateTentId(models.Model):
     _name = "product.template.many2one.field"
     _description = "Add many2one field for my module"
     _inherit = ['product.template']
-
    
