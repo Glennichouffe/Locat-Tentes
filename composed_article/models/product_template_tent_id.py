4ad95a13f1a30a38b9783180cbc01f3e75b7c648
Glenn <mr.glennichou@gmail.com>
2021-09-02 19:29:51 +0200

    structure changes

--- composed_article/models/product_template_tent_id.py
+++ composed_article/models/product_template_tent_id.py
@@ -2,9 +2,7 @@ from odoo import api, fields, models
 
 class ProductTemplateTentId(models.Model):
 
-    _name = "product.template.many2one.field"
-    _description = "Add many2one field for my module"
-    _inherit = ['product.template']
+    _inherit = 'product.template'
 
     articles_tent_id = fields.Many2one('tent.article', 'TentID')
    
