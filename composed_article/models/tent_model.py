fb7e7100ef88e2d674bec50dcec820fc3c485944
Glenn <mr.glennichou@gmail.com>
2021-09-02 22:11:53 +0200

    View modifications

--- composed_article/models/tent_model.py
+++ composed_article/models/tent_model.py
@@ -7,11 +7,11 @@ class TentArticle(models.Model):
 
     article_name = fields.Char(string='Nom', required=True, tracking=True)
     tent_width = fields.Integer(string='Largeur de la tente', required=True)
-    armatures_amount = fields.Integer(string = 'Armatures disponibles', required=True)
-    pignons_amount = fields.Integer(string = 'Pignons disponibles', required=True)
-    note = fields.Text(string='Description')
+    armatures_amount_total = fields.Integer(string = 'Nombre total d\armatures', required=True)
+    pignons_amount_total = fields.Integer(string = 'Nombre total de pignons', required=True)
+    note = fields.Text(string='Notes éventuelles')
     image = fields.Binary(string="Image")
-    amount_available = fields.Integer(string='Quantité encore possible à sous-louer', compute='_compute_maximum_quantity')
+    amount_available = fields.Integer(string='Quantité maximale de tentes possibles à louer', compute='_compute_maximum_quantity')
     active = fields.Boolean(string="Active", default=True)
     article_composed_ids = fields.One2many('product.template', 'articles_tent_id', string="Articles composants")
     price_week_without_installation = fields.Float('Prix pour une semaine de location sans installation',  required=True)
@@ -19,6 +19,8 @@ class TentArticle(models.Model):
     rented_tents = fields.Integer('Nombre de tentes louées', readonly = True, default = 0)
     rented_armatures = fields.Integer('Nombre d\'armatures louées', readonly = True, default = 0)
     rented_pignons = fields.Integer('Nombre de pignons loués', readonly = True, default = 0)
+    armatures_amount_available= fields.Integer(string = 'Armatures disponibles', default = self.rec. armatures_amount_total)
+    pignons_amount_available = fields.Integer(string = 'Pignons disponibles', default = self.rec.pignon_amount_total)
     def _compute_maximum_quantity(self):
         for rec in self:
            rec.amount_available = rec.armatures_amount / 3
