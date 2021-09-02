994a006e2f5d0dc33fdbaa8abc1505738fc6bba2
Glenn <mr.glennichou@gmail.com>
2021-09-03 00:10:21 +0200

    product_id

--- invoice_price_double/models/price_with_installation.py
+++ invoice_price_double/models/price_with_installation.py
@@ -7,3 +7,4 @@ class PriceWithInstallation(models.Model):
     _inherit = "product.template"
 
     price_with_installation = fields.Float(string='Prix avec l\'installation')
+    has_second_price = fields.Boolean(string='A un prix d\'installation', default = True)
