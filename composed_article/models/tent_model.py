6cba4c0ed8bb4bffc878392d1f081d7ddd98bcd6
Glenn <mr.glennichou@gmail.com>
2021-09-02 23:08:57 +0200

    typo

--- composed_article/models/tent_model.py
+++ composed_article/models/tent_model.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
++from odoo import api, fields, models
 
 class TentArticle(models.Model):
 
@@ -20,7 +20,7 @@ class TentArticle(models.Model):
     rented_armatures = fields.Integer('Nombre d\'armatures louées', readonly = True, default = 0)
     rented_pignons = fields.Integer('Nombre de pignons loués', readonly = True, default = 0)
     armatures_amount_available= fields.Integer(string = 'Armatures disponibles', default = lambda self: self.armatures_amount_total)
-    pignons_amount_available = fields.Integer(string = 'Pignons disponibles', default = lambda self: self.pignon_amount_total)
+    pignons_amount_available = fields.Integer(string = 'Pignons disponibles', default = lambda self: self.pignons_amount_total)
     
     def _compute_maximum_quantity(self):
         for rec in self:
